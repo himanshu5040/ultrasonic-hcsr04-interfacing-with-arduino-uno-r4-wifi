3b4a3cecba9e1c209acfe226972f0e1c920a11c0
Himanshu <himanshu.shakya_cs.aiml24@gla.ac.in>
2026-02-03 09:19:43 +0530

    addpin configuration in ultrasonic sensor

--- src/main.cpp
+++ src/main.cpp
@@ -8,5 +8,17 @@ pinMode(trigPin, OUTPUT);
 }
 
 void loop() {
-// write your code here
+     digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    long duration = pulseIn(echoPin, HIGH);
+    long distance = duration * 0.034/2;
+    Serial.println("distance:");
+    Serial.println(distance);
+    Serial.println("cm");
+    dalay(1000);
+
+
 }
